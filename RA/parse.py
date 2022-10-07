2f2338bbcf12af79f699e486228de63131203925
Yaxin(Janet) Zhuang <49347418+yqz5514@users.noreply.github.com>
2022-10-07 18:14:06 -0400

    update

--- RA/parse.py
+++ RA/parse.py
@@ -40,36 +40,50 @@ sum(1 for _ in re.finditer(pattern_WKU,text))
 # %%
 type(match.group())
 #%%
-pattern_ISD = re.compile(r'ISD\s\s\d+')
+pattern_ISD = re.compile(r'ISD\s\s(1976)\d+')
 matches = pattern_ISD.finditer(text)
 for match in matches:
-    print(match.group()) 
+    print(match.group()) # same value for 19760106_wk1
+#%%
+ISD = []
+ISD.append(match.group())
 #%%
 sum(1 for _ in re.finditer(pattern_ISD,text))
 #8363
 #becasue there are many ISD value besides the one associate with publish time
-#probably can check see if only publish time start with 1976 then we can parse it by that
-#if not, find shared pattern
+#publish time start with 1976 #1379
 #%%%
-pattern_ABS = re.compile(r'ABST\s.+\s.+[A-Z]$')
-matches = pattern_ABS.finditer(text)
-for match in matches:
-    print(match.group())
+#pattern_ABS = re.compile(r'ABST\s.+\s.+[A-Z]$')
+#matches = pattern_ABS.finditer(text)
+#for match in matches:
+ #   print(match.group())
 
 
 #%%
-pattern_ABS = re.compile(r'ABST*\n*((?:\n.*)+?)(?=\n[A-Z]{4}|\Z)')
+pattern_ABS = re.compile(r'ABST\n*\n*((?:\n.*)+?)(?=\n[A-Z]{4}|\Z)')
 matches = pattern_ABS.finditer(text)
-for match in matches:
-    print(match.group())     
+#for match in matches:
+#    print(match.group())     
 #    .*\n*: Match rest of the line followed by 0 or more line breaks
 #((?:\n.*)+?): Capture group 1 to capture our text which 1 or lines of everything until next condition is satisfied
 #(?=\nAREA:|\Z): Assert that we have a line break followed by AREA: or end of input right ahead of the current position
+
+#%%
+ABST = []
+for match in matches:
+    val = match.group()
+    ABST.append(val)
+#print(ABST)
+#%%
+#abst = pd.DataFrame({'ABST':ABST})
+#abst.to_excel('abst_check.xlsx')
 #%%
 sum(1 for _ in re.finditer(pattern_ABS,text))
 # Search for 'ABST' 1379
 # search in text file by case is 1379
-# search by paragraph with 1384 record
+# search by paragraph with 1384 
+# add'\n' after ABST match #1379
+#%%
 
 #%%
 #test
@@ -79,15 +93,26 @@ with open('test.txt', 'r') as f:
 #%%
 pattern_ABS = re.compile(r'par')
 matches = pattern_ABS.finditer(text1)
-for match in matches:
-    print(match.group())     
-    
-    
-    
+#for match in matches:
+#    print(match.group())     
+#%%
+sum(1 for _ in re.finditer(pattern_ISD,text))
+
+#%%
+#ICL
+pattern_ICL = re.compile(r'ICL\s\s\w+')
+matches = pattern_ICL.finditer(text)
+#for match in matches:
+#    print(match.group())
+#%%
+sum(1 for _ in re.finditer(pattern_ICL,text))
+# search ICL\s\s\w+ #2023
+# some aplication has multiple line of ICL info
+
     
 # %%
-result_1976 = pd.DataFrame({'WKU':abs_match })
-
+result_1976 = pd.DataFrame({'Pattent ID':abs_match, 'Publish Date':ISD, 'Abstract':ABST})
+result_1976.to_excel('demo.xlsx')
 #%%
 print(result_1976.head())
 # %%

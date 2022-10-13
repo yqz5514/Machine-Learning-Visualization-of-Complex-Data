f3d9ead4681cfc48261654ea6cd1fb6c75dc8b7b
Yaxin(Janet) Zhuang <49347418+yqz5514@users.noreply.github.com>
2022-10-12 21:25:25 -0400

    Update Lab3.py

--- Vis_practice/Lab3.py
+++ Vis_practice/Lab3.py
@@ -1,17 +1,98 @@
 #Lab3
+#%%
+#library
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
 
+
+#%%
 #1. Load the dataset using pandas package. Clean the dataset by removing the ‘nan’ and missing data.
+ucl = 'https://raw.githubusercontent.com/rjafari979/Information-Visualization-Data-Analytics-Dataset-/main/CONVENIENT_global_confirmed_cases.csv'
+df = pd.read_csv(ucl)
+#%%
+df.head()
+#%%
+df.isnull().sum()
+#%%
+df.dropna(how ='any', inplace = True)
+#%%
+df.head()
+#%%
+df.isnull().sum()
+#%%
 missing_df = df.isnull().sum(axis=0).reset_index()
 missing_df.columns = ['variable', 'missing values']
 missing_df['filling factor (%)']=(df.shape[0]-missing_df['missing values'])/df.shape[0]*100
 missing_df.sort_values('filling factor (%)').reset_index(drop = True)
+
+#%%
+
+
+#%%
 #2. The country “China” has multiple columns (“ China.1”, “China.2”, …) . 
-
 # Create a new column name it “China_sum” which contains the sum of “China.1” + “China.2”, … column wise. 
-
 # You can use the following command to perform the task:
-#3. Repeat step 2 for the “United Kingdom”.
+print(df.columns.values.tolist())
+#%%
+df.iloc[0:,58:90]
+#%%
+df['China_sum'] = df.iloc[0:,58:90].astype(float).sum(axis=1)
+#%%
+##3. Repeat step 2 for the “United Kingdom”.
+
+df.iloc[0:,250:260]
+#%%
+df['UK_sum'] = df.iloc[0:,250:260].astype(float).sum(axis=1)
+#%%
+df['China']
+
+
+#%%
 #4. Plot the COVID confirmed cases for the following US versus the time. The final plot should look like bellow.
+
+df.dtypes
+#%%
+df.columns
+#%%
+df['Country/Region'] = pd.to_datetime(df['Country/Region'])
+#%%
+df_us = df[['Country/Region','US']]
+
+#%%
+# resampling
+resampled_month =df_us.resample('M', on='Country/Region').mean().reset_index(drop=False)
+#%%
+resampled_month
+#%%
+y1=['US','United Kingdom','China','Germany','Brazil','India','Italy']
+plt.figure(figsize=(16, 8))
+df.plot(x = 'Country/Region', y = y1, label = "US" )
+#df.plot(x = 'Country/Region', y = 'United Kingdom', label = "United Kingdom")
+#df.plot(x = 'Country/Region', y = 'China', label = "China")
+#df.plot(x = 'Country/Region', y = 'Germany', label = "Germany")
+#df.plot(x = 'Country/Region', y = 'Brazil', label = "Brazil")
+#df.plot(x = 'Country/Region', y = 'India', label = "India")
+#df.plot(x = 'Country/Region', y = 'Italy', label = "Italy")
+
+plt.title('GLobal confirmed cases')
+# adding Label to the x-axis
+plt.ylabel('Confirmed Covid-10 cases')
+plt.xlabel('Year')
+#plt.xticks(['Feb2020','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov'])
+plt.tight_layout()
+#plt.grid(10)
+# adding legend to the curve
+plt.legend()
+#%%
+df_11 = df[['Country/Region','US','UK_sum','China_sum','Germany','Brazil','India','Italy']]
+#%%
+ax = df_11.plot(x = 'Country/Region')
+
+# Additional customizations
+ax.set_xlabel('Date')
+ax.legend(fontsize=12)
+#%%
 #5. Repeat step 4 for the “United Kingdom”, “China”, ”Germany”, ”Brazil”, “India” and “Italy”. 
 # The final plot should look like bellow.
 #6. Plot the histogram plot of the graph in Question 4.
@@ -45,3 +126,4 @@ def get_stats(group):
 global_stats = df['DEPARTURE_DELAY'].groupby(df['AIRLINE']).apply(get_stats).unstack()
 global_stats = global_stats.sort_values('count')
 global_stats
+# %%

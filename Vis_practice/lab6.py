22a5618205c80150ad5373424b9add6f7576cdb7
Yaxin(Janet) Zhuang <49347418+yqz5514@users.noreply.github.com>
2022-12-15 12:03:00 -0500

    Update lab6.py

--- Vis_practice/lab6.py
+++ Vis_practice/lab6.py
@@ -13,106 +13,111 @@ df_fel = df[df['Gender']=='Female']
 # %%
 df_fel.head()
 # %%
-df_fel.shape
-# %%
 df_fel = df_fel[0:100]
 # %%
-df_fel.columns
+df_fel.reset_index(inplace=True)
 # %%
 df_fel[['Height','Weight']] = round(df_fel[['Height','Weight']], 2)
 # %%
+df_fel.drop(columns='index', inplace=True)
+#%%
 df_fel.head()
+#%%
+df_fel.plot(kind = 'line',
+            figsize=(12,6),
+            title='Height and weight vs number of observation',
+            xlabel='number of observation',
+            ylabel='value')
+#%%
+df_fel.plot(kind = 'hist',
+            figsize=(12,6),
+            title='Height and weight vs number of observation',
+            
+            )
 # %%
-fig, axes = plt.subplots(nrows=1, ncols=2,sharex=True, sharey=True)
-df_fel.hist(ax=axes)
-plt.suptitle('Felmale Height and Wieght distribution',ha='center', fontsize='xx-large')
-fig.text(0.04, 0.5, 'Freq', va='center', rotation='vertical')
-fig.text(0.5, 0.04, 'Value under each category', ha='center')
+# fig, axes = plt.subplots(nrows=1, ncols=2,sharex=True, sharey=True)
+# df_fel.hist(ax=axes)
+# plt.suptitle('Felmale Height and Wieght distribution',ha='center', fontsize='xx-large')
+# fig.text(0.04, 0.5, 'Freq', va='center', rotation='vertical')
+# fig.text(0.5, 0.04, 'Value under each category', ha='center')
 
 # %%
 #z-transform
 df_z = ((df_fel[['Height','Weight']] - df_fel[['Height','Weight']].mean())/df_fel[['Height','Weight']].std())
-df_z[['Height','Weight']].plot()
-plt.show()
+
 #%%
 df_z[['Height','Weight']] = round(df_z[['Height','Weight']], 2)
 
 #%%
 df_z.head()
 # %%
-df_z[['Height','Weight']].hist()
+df_z.plot(kind = 'line',
+            figsize=(12,6),
+            title='Height and weight vs number of observation for transformed data',
+            xlabel='number of observation',
+            ylabel='value')
 #%%
-import numpy as np
-#outlier
-q1_d, q3_d = np.percentile(df_z['Height'], [25,75])
-q1_a, q3_a = np.percentile(df_z['Weight'], [25,75])
-
-
-IQR_d = q3_d - q1_d
-IQR_a = q3_a - q1_a
-
-
-
-low_outlier_d = q1_d - 1.5*IQR_d
-high_outlier_d = q3_d + 1.5*IQR_d
-
-low_outlier_a = q1_a - 1.5*IQR_a
-high_outlier_a = q3_a + 1.5*IQR_a
-
-# %%
-outlier_table = {'Outlier Boundary': ['low limit','high limit'],
-                 'Height':[low_outlier_d, high_outlier_d],
-                 'Weight':[low_outlier_a, high_outlier_a],}
-
-df_outlier_table = pd.DataFrame(outlier_table)
-print(df_outlier_table)
-#%%
-print(f'IQR of height of female is {IQR_d}')
-print(f'IQR of weight of female is {IQR_a}')
-print(f'Any height lower than {low_outlier_d:.2f} inches and higher than {high_outlier_d:.2f} inches consider as outlier')
-print(f'Any weight lower than {low_outlier_a:.2f} inches and higher than {high_outlier_a:.2f} inches consider as outliers')
-
-# %%
-sns.boxplot(data = df_z)
-plt.show()
-# %%
-df_remove_out = df_z.copy()
-# %%
-df_remove_out['Height'] = df_remove_out[(df_remove_out['Height']>=-2.6175)&(df_remove_out['Height']<=2.6425)]
-#%%
-remove_out_weight = df_remove_out[(df_remove_out['Weight']>=-2.23)&(df_remove_out['Weight']<=2.27)]
-
-#%%
-sns.boxplot(data = remove_out_weight
+df_z.plot(kind = 'hist',
+            figsize=(12,6),
+            title='Height and weight vs number of observation for transformed data',
             )
+#%%
+fig, axes = plt.subplots(nrows=2, ncols=2)
+df_fel.plot(kind = 'line',
+            figsize=(12,6),
+            title='Height and weight vs number of observation',
+            xlabel='number of observation',
+            ylabel='value',
+            ax = axes[0,0])
+df_z.plot(kind = 'line',
+            figsize=(12,6),
+            title='Height and weight vs number of observation for transformed data',
+            xlabel='number of observation',
+            ylabel='value',
+            ax=axes[0,1])
+df_fel.plot(kind = 'hist',
+            figsize=(12,6),
+            title='Height and weight vs number of observation',
+            ax=axes[1,0]
+            )
+df_z.plot(kind = 'hist',
+            figsize=(12,6),
+            title='Height and weight vs number of observation for transformed data',
+            ax=axes[1,1])
+plt.tight_layout()
 plt.show()
-# %%
+#%%
+more_170 = len(df_fel[df_fel['Weight']>170])/len(df_fel)*100
+taller_66 = len(df_fel[df_fel['Height']>66])/len(df_fel)*100
+#%%
+print(f'Sample mean of the ladies weight is {df_fel.Weight.mean():.2f} lb')
+print(f'Sample mean of the ladies height is {df_fel.Height.mean():.2f} lb')
+print(f'Sample std of the ladies weight is {df_fel.Height.std():.2f} lb')
+print(f'Sample std of the ladies weight is {df_fel.Weight.std():.2f} lb')
+print(f'Sample median of the ladies weight is {df_fel.Height.median():.2f} lb')
+print(f'Sample median of the ladies weight is {df_fel.Height.median():.2f} lb')
+print(f'The probability that a lady weight more than 170lb is {more_170:.2f} %')
+print(f'The probability that a lady Height more than 66 inches is {taller_66:.2f} %')
+#%%
 import statsmodels
 from statsmodels.graphics.gofplots import qqplot
+
 plt.figure()
 qqplot(df_fel['Height'],line='s')
 plt.title('Height of female')
 plt.show()
-# %%
+#%%
 plt.figure()
 qqplot(df_fel['Weight'],line='s')
 plt.title('Weight of female')
 plt.show()
-# %%
-plt.figure()
-qqplot(df_z['Weight'],line='s')
-plt.title('Weight of female(z-transformed)')
-plt.show()
-plt.figure()
-qqplot(df_z['Height'],line='s')
-plt.title('Height of female(z-transformed)')
-plt.show()
+
 # %%
 import numpy as np
 from scipy.stats import shapiro
 from scipy.stats import kstest
 from scipy.stats import normaltest
-from scipy.stats import rankdata
+
 def shapiro_test(x, title):
     stats, p = shapiro(x)
     print('=' * 50)
@@ -151,37 +156,85 @@ def da_k_squared_test(x, title):
     else:
         print(f'da_k_squared test : {title} dataset is Not Normal')
     print('=' * 50)
-    
+
 #%%
    #========================
 #= Normality test Shapiro
 #========================
 
-shapiro_test(df_fel['Height'],'raw data')
-shapiro_test(df_z['Height'],'Transformed data')
-shapiro_test(df_fel['Weight'],'raw data')
-shapiro_test(df_z['Weight'],'Transformed data')
+shapiro_test(df_fel['Height'],'Height felmale data')
+
+shapiro_test(df_fel['Weight'],'Weight female data')
+
 #%%
 #========================
 #= ks_test test Shapiro
 #========================
-ks_test(df_fel['Height'],'raw data')
-ks_test(df_z['Height'],'Transformed data')
-ks_test(df_fel['Weight'],'raw data')
-ks_test(df_z['Weight'],'Transformed data')
+ks_test(df_fel['Height'],'Height felmale data')
+
+ks_test(df_fel['Weight'],'Weight female data')
+
 #%%
 #========================
 #= da_k_squared_test
 # #========================
-print(100*'=')
-da_k_squared_test(x,'raw data')
-da_k_squared_test(y,'Transformed data')
-print(100*'=')
-mean = 194
-std = 11.2
-L = 175
-U = 225
-print(f'the probability that '
-      f'the observation between '
-      f'{U} and {L} is '
-      f'{(st.norm(mean,std).cdf(U)-st.norm(mean,std).cdf(L))*100:.2f}%') 
+
+da_k_squared_test(df_fel['Height'],'Height felmale data')
+da_k_squared_test(df_fel['Weight'],'Weight female data')
+#%%
+import numpy as np
+#outlier
+q1_d, q3_d = np.percentile(df_fel['Height'], [25,75])
+q1_a, q3_a = np.percentile(df_fel['Weight'], [25,75])
+
+
+IQR_d = q3_d - q1_d
+IQR_a = q3_a - q1_a
+
+
+
+low_outlier_d = q1_d - 1.5*IQR_d
+high_outlier_d = q3_d + 1.5*IQR_d
+
+low_outlier_a = q1_a - 1.5*IQR_a
+high_outlier_a = q3_a + 1.5*IQR_a
+
+# %%
+outlier_table = {'Outlier Boundary': ['low limit','high limit'],
+                 'Height':[low_outlier_d, high_outlier_d],
+                 'Weight':[low_outlier_a, high_outlier_a],}
+
+df_outlier_table = pd.DataFrame(outlier_table)
+print(df_outlier_table)
+#%%
+print(f'Q1 of height of female is {low_outlier_d:.2f}, Q3 is {high_outlier_d:.2f}')
+print(f'IQR of height of female is {IQR_d:.2f}')
+print(f'Any height lower than {low_outlier_d:.2f} inches and higher than {high_outlier_d:.2f} inches consider as outlier')
+#%%
+sns.boxplot(df_fel.Height)
+#%%
+df_rem_h = df_fel[(df_fel['Height']>=56.11) &(df_fel['Height']<=71.23)]
+
+#%%
+sns.boxplot(df_rem_h.Height)
+#%%
+df_fel.shape
+#%%
+df_rem_h.shape
+
+#%%
+print(f'Q1 of weight of female is {low_outlier_a:.2f}, Q3 is {high_outlier_a:.2f}')
+print(f'IQR of weight of female is {IQR_a:.2f}')
+print(f'Any weight lower than {low_outlier_a:.2f} and higher than {high_outlier_a:.2f} consider as outliers')
+
+#%%
+sns.boxplot(df_fel.Weight)
+
+# %%
+df_remove_out = df_fel[(df_fel['Weight']>=90.94) &(df_fel['Weight']<=178.07)]
+
+#%%
+sns.boxplot(df_remove_out.Weight
+            )
+plt.show()
+    
